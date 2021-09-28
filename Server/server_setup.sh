3a96a3a9280c9d7b41a29d477e4e32f8e7ca41f5
Klaus Hammer <k.n.hammer@hotmail.com>
2021-09-28 16:11:34 +0200

    Added client and server folder + setup script

---
+++ Server/server_setup.sh
@@ -0,0 +1,13 @@
+# Installs all the dependencies that the master requires.
+
+
+# Install and update standard packages
+sudo apt update
+sudo apt -y upgrade
+
+# Add new packages to install in the list below
+sudo apt -y install git 
+
+
+
+
