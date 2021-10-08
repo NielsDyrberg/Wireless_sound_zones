cfd92134570c6b85c9eba0d302fc207199a078f3
Niels Dyrberg <ncpd@live.dk>
2021-10-08 14:37:23 +0200

    added_send_too

--- shared_modules/soundzone_protocol/szp_modules/X01_send.py
+++ shared_modules/soundzone_protocol/szp_modules/X01_send.py
@@ -1,4 +1,6 @@
 
+from .bi_classes import *
+
 
 LEN_TIME = 8
 
@@ -9,8 +11,8 @@ class X01Send:
 
     This class is used when sending a package containing a sound chunk. It encodes both time and sound data.
 
-    Atributes
-    ---------
+    Attributes
+    ----------
     time: Time
         An object containing time data.
     payload: list of ints
@@ -27,7 +29,7 @@ class X01Send:
         """
         Initiates class variables
         """
-        self.time = Time()
+        self.time = TimeEncoding()
         self.payload = []
 
     def encode(self) -> str:
@@ -47,63 +49,7 @@ class X01Send:
         :return: None
         """
         time_buffer = buffer[:LEN_TIME]
-        self.time = Time()
+        self.time = TimeEncoding()
         self.time.decode(time_buffer)
 
         self.payload = [((x << 8) + y) for x, y in zip(buffer[LEN_TIME::2], buffer[LEN_TIME+1::2])]
-
-
-class Time:
-    """
-    A class containing time data, used for timing playback.
-
-    Atributes
-    ---------
-    minute: The minute the chunk should be played
-    second: The second the chunk should be played
-    mili_second: The mili_second the chunk should be played
-    micro_second: The micro_second the chunk should be played
-    nano_second: The nano_second the chunk should be played
-
-    Methods
-    -------
-    __init__(minute=None, second=None, mil_second=None, mic_second=None, n_second=None)
-    encode()
-        Encodes time data into a hex string.
-    decode(buffer)
-        Decodes a hex string, and fills it into the class variables
-    """
-    def __init__(self, minute=None, second=None, mil_second=None, mic_second=None, n_second=None):
-        """
-        Initiates class variables
-        :param minute: The minute the chunk should be played
-        :param second: The second the chunk should be played
-        :param mil_second: The mili_second the chunk should be played
-        :param mic_second: The micro_second the chunk should be played
-        :param n_second: The nano_second the chunk should be played
-        """
-        self.minute = minute
-        self.second = second
-        self.mili_second = mil_second
-        self.micro_second = mic_second
-        self.nano_second = n_second
-
-    def encode(self) -> str:
-        """
-        Encodes time data into a hex string.
-        :return: Encoded hex string
-        """
-        return "{:02X}{:02X}{:04X}{:04X}{:04X}".format(self.minute, self.second, self.mili_second, self.micro_second,
-                                                       self.nano_second)
-
-    def decode(self, buffer):
-        """
-        Decodes a hex string, and fills it into the class variables
-        :param buffer: List of ints
-        :return: None
-        """
-        self.minute = buffer[0]
-        self.second = buffer[1]
-        self.mili_second = (buffer[2] << 8) + buffer[3]
-        self.micro_second = (buffer[4] << 8) + buffer[5]
-        self.nano_second = (buffer[6] << 8) + buffer[7]
